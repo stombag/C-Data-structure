ad1e1f129df49934ff13a0ae77838cd4abb1d079
민혁 김 <ngl570202@gmail.com>
2025-05-25 20:37:33 +0900

    5.25 연결리스트 개선

--- C++Data structure/LinkedList.cpp
+++ C++Data structure/LinkedList.cpp
@@ -2,195 +2,111 @@
 #include "LinkedList.h"
 using namespace std;
 
-Node* Create(DataType data)
+void LinkedList::Push(DataType data)
 {
-	Node* newNode = new Node();
-	// Node*의 별명은 newnode이고 new Node의 주소값을 가리키는 건 newnode이다 
-	// 포인터는 주소만 가리키는 자료형이다
-	newNode->Data = data;
-	// 주소값을 가리키는 newNode에 접근하면 Node가 나오는데 그것에 있는 Data에 data값을 넣는다.
-	newNode->NextNode = nullptr;
-	
-	// NextNode를 nullptr로 비우는 이유는 다음 노드의 주소를 넣기 위해서 이다. 
-	// nullptr는 아무것도 가리키지 않는 포인터 키워드이다.
-	return newNode;
-	// newNode가 가리키는 주소값을 반환한다 즉 그 주소값의 위치에 값을 넣는다. 
-}
-Node* DummyHead()
-{
-	Node* dummyhead = new Node();
-	dummyhead->NextNode = nullptr;
-	return dummyhead;
-	// 기본 노드에서 내용이 비어 있는 상태의 노드를 만들고 만드는 노드 앞에 세우면 그게 더미헤드가 된다.
-	// main 첫 줄에 넣기, 이렇게 따로 더미 헤드라는 함수를 만들어야한다. 
-}
-void Destroy(Node* node)
-{
-	delete node;
-	// 메모리 해제 
-	node = nullptr;
-	// node이 가지고 있는 주소값없애기 
+	Node* newNode = new Node(data);
+	Node* tail = dummyHead;
+	while (tail->NextNode != nullptr)
+		tail = tail->NextNode;
+	tail->NextNode = newNode;
 
 }
-void Push(Node** head, Node* node)
-//Node** head : 외부에서 head가 가리키는 위치를 변경할 수 있다, 연결 리스트의 시작 주소를 바꿀 수 있다. 
-//Node* node : 리스트에 추가할 새노드의 포인터이다.
-// *head 연결 리스트 시작점 
+
+void LinkedList::Insert(int index, DataType data)
 {
-	if ((*head) != nullptr)
+	Node* newNode = new Node(data);
+	Node* current = dummyHead;
+
+	while (index-- > 0 && current->NextNode != nullptr)
 	{
-		//head가 비어 있는 않는 경우
-		Node* tail = (*head);
-		// tail은 현재 리스트의 시작점 복사해서 사요한다. 맨 앞부터 출발
-
-		while (tail->NextNode != nullptr)
-			tail = tail->NextNode;
-		// NextNode가 nullptr이 될 때가지 리스트의 끝까지 이동
-		tail->NextNode = node;
-		// 리스트의 마지막 노드의 next에 새로운 노드를 연결한다. 
-
+		current = current->NextNode;
 	}
-	else
+	newNode->NextNode = current->NextNode;
+	current->NextNode = newNode;
+
+}
+
+void LinkedList::Remove(int index)
+{
+	Node* current = dummyHead;
+	while (index-- > 0 && current->NextNode != nullptr)
 	{
-		*head = node;
-		// node를 직접 연결 
+		current = current->NextNode;
 	}
 
-}
-
-void Insert(Node* current, Node* node)
-{
-	//current : 삽입할 위치 바로 앞 노드
-	//node : 삽입할 새 노드 
-	node->NextNode = current->NextNode;
-	// 새 노드가 기존 다음 노드를 가리키게 한다.
-	current->NextNode = node;
-	// 현재 노드의 다음 새노드로 변경한다. 
-}
-
-/*void InsertHead(Node** current, Node* head)
-{
-}
-*/
-
-void Remove(Node** head, Node* remove)
-{
-	//Node**head : dummyHead의 NextNode가 저장된 메모리위치(주소)를 가리킨다.
-	// Node** head 는 &dummyHead->NextNode 와 같다. 
-	// Node*remove :제거할 노드를 직접 지정하다. 제거할 targer이다.
-	if (*head == remove)
+	Node* target = current->NextNode;
+	if (target != nullptr)
 	{
-		//리스트의 첫번째 노드가 제거 대상일 경우 
-		*head = remove->NextNode;
-		// 위에 것과 dummyHead -> NextNode = remove-> NextNode;와 같다. 
-		//월래는 더미헤드 NextNode가 첫번째 노드였는데 두번째 노드로 변경하는거다
-	}
-	else
-	{
-		Node* current = *head;
-		// current는 dummyHead->NextNode에서 시작한다. 
-		while (current != nullptr && current->NextNode != remove)
-			current = current->NextNode;
-		//remove 바로 이전 노드를 찾기 위한 반복문 
-		// current -> NextNode == remove가 될때 까지 이동
-		// 찾으면 current는 제거 대상 앞의 노드이다.
+		current->NextNode = target->NextNode;
+		delete target;
 
-		if (current != nullptr)
-			current->NextNode = remove->NextNode;
-		// 제거 대상 노드를 리스트에서 건너뛴다.
-		// current가 이제 remove를 건너뛰고 remove->NextNode를 가리킨다. 
 	}
 }
 
-Node* GetNode(Node* head, int index)
+DataType LinkedList::Get(int index)
 {
-	//Node* head : 리스트의 시작 노드 (더미헤드는 제외하고 첫 노드부터 탐색)
-	// int index : 찾고 싶은 인덱스 번호
+	Node* current = dummyHead->NextNode;
+	while (index-- > 0 && current != nullptr)
+	{
+		current = current->NextNode;
 
-	Node* current = head;
-	//head가 가리키는 노드를 가리킨다.
-	while (current != nullptr && (--index >= 0))
-	// current != nullptr : 현재 가리키는 노드가 존재할 때만 이동한다.
-	// index는 몇 칸 이동해야하는지 의미이며 --로 1씩 줄어든다.5 4 3 2 1
-	// index번째 위치에 있는 노드를 찾기 위해 리스트를 앞으로 한 칸씩 이동한다. 
-
-	current = current->NextNode;
-	// 다음 노드로 이동한다.
-		return current;
+	}
+	if (current == nullptr) throw out_of_range("Index out of range");
+	return current->Data;
 }
 
-int GetNodeCount(Node* head)
+int LinkedList::Count() const
 {
-	//Node* head : 리스트의 시작 노드(더미헤드의 다음 노드) 
 	int count = 0;
-	Node* current = head;
-
+	Node* current = dummyHead->NextNode;
 	while (current != nullptr)
 	{
-		// 리스트 끝가지 순회한다. 
-		current = current->NextNode;
 		count++;
-		// 다음 노드로 이동하고 숫자를 1 더한다.
-
-			
+		current = current->NextNode;
 	}
+
 	return count;
 }
 
+void LinkedList::print() const
+{
+	Node* current = dummyHead->NextNode;
+	while (current != nullptr)
+	{
+		cout << current->Data << " -> ";
+		current = current->NextNode;
+
+	}
+	cout << "NULL" << endl;
+}
+
+void LinkedList::Clear()
+{
+	Node* current = dummyHead->NextNode;
+	while (current != nullptr)
+	{
+		Node* temp = current;
+		current = current->NextNode;
+		delete temp;
+
+	}
+	dummyHead->NextNode = nullptr;
+}
 
 int main()
 {
-	Node* head = DummyHead();
-	cout << "DummeyHead" << endl;
-	Node* a = Create(1);
-	cout << "Node a" << a->Data<< endl;
-	Push(&head->NextNode, a);
-	Node* b = Create(2);
-	cout << "Node b" << b->Data<< endl;
-	Push(&a->NextNode, b);
-	Node* c = Create(3);
-	cout << "Node c" << c->Data<< endl<<endl;
-	Push(&b->NextNode, c);
-	Node* d = Create(4);
-	cout << "Node d" << d->Data<< endl<<endl;
-	Push(&c->NextNode, d);
+	LinkedList list;
+	list.Push(10);
+	list.Push(20);
+	list.Push(30);
+	list.print();
 
-	int count = GetNodeCount(head->NextNode);
-	cout << count << endl;
+	list.Insert(1, 49);
+	list.print();
 
-	Node* GGetNdoe = GetNode(head->NextNode, 1);
-	cout << GGetNdoe->Data << endl;
+	list.Remove(2);
+	list.print();
 
-	Remove(&head->NextNode, d);
-	cout << count << endl;
-
-	Node* E = Create(5);
-	cout << "Node E" << E->Data<< endl<<endl;
-	Push(&c->NextNode, E);
-	
-	count = GetNodeCount(head->NextNode);
-	cout << count << endl;
-
-
-	 // 10. 전체 리스트 출력
-    cout << "Step 10: Final list: ";
-    Node* current = head->NextNode;
-    while (current != nullptr) {
-        cout << current->Data << " ";
-        current = current->NextNode;
-    }
-    cout << endl;
-
-    // 11. 모든 노드 메모리 해제
-    current = head->NextNode;
-    while (current != nullptr) {
-        Node* next = current->NextNode;
-        Destroy(current);
-        current = next;
-    }
-    Destroy(head);
-    cout << "Step 11: All nodes destroyed." << endl;
-
-    return 0;
-
+	cout << "2" << list.Get(2) << endl;
+	cout << "노드 수" << list.Count() << endl;
 }
--- C++Data structure/LinkedList.h
+++ C++Data structure/LinkedList.h
@@ -6,43 +6,45 @@ typedef int DataType;
 // 자료형 int를 DataType이라는 걸로 이름만 바꾸었다. 기능은 int와 동일하다
 // 사용하는 이유 : int 말고 다른 자료형 float 같은 걸로 바꿀때 여기만 바꾸면 된다., 또한 DataType이라고 쓰면 연결리스트에만 사용하는 데이터의 타입인걸 알 수 있다.
 
-struct Node
+class LinkedList
 {
-// 노드 구조체 만들기 
-	DataType Data;
-	// 노드의 데이터를 저장할 곳
-	Node* NextNode;
-	// 다음 노드의 주소를 저장하는 포인터
-	// 현재 노드가 간리키는 주소는 다음 노드의 위치이다. 다음 노드의 주소를 저장한다.
+private:
 
+	struct Node
+	{
+		// 노드 구조체 만들기 
+		DataType Data;
+		// 노드의 데이터를 저장할 곳
+		Node* NextNode;
+		// 다음 노드의 주소를 저장하는 포인터
+		// 현재 노드가 간리키는 주소는 다음 노드의 위치이다. 다음 노드의 주소를 저장한다.
+
+		Node(DataType d = 0) : Data(d), NextNode(nullptr) {}
+
+	};
+	Node* dummyHead;
+
+public:
+	LinkedList()
+	{
+		dummyHead = new Node();
+	}
+	~LinkedList()
+	{
+		Clear();
+		delete dummyHead;
+	}
+	void Push(DataType data);
+
+	void Insert(int index, DataType data);
+
+	void Remove(int index);
+
+	DataType Get(int index);
+
+	int Count() const;
+
+	void print() const;
+
+	void Clear();
 };
-Node* Create(DataType data);
-// 함수 node 구조체의 포인터를 반환함을 나타난다.
-// 만든 노드를 구조체에 넣어야 한다. 기억?
-// data는 나중에 구조체에 Data에 넣을 값이다. 
-void Destroy(Node* node);
-// void는 함수가 값을 반환하지 않을때 사용한다. 
-// void는 구조체에 넣을 필요가 없다. 행동?
-// Node* node는 삭제할 노드 넣는거다.
-void Push(Node** Head, Node* node);
-//Node**는 Node*를 가르킨다. 즉 Node** head는 Node* 타입의 포인터를 가리키"는 포인터 변수 이다.  
-//연결 리스트의 헤드 포인터를 변경하려면 원래의 포인터 자체를 변경해야 한다. 이를 위해 포인터의 주소를 전달하는, 즉 포인터에 대한 포인터(**)를 사용한다.
-void Insert(Node* current, Node* node);
-// Node* current는 새로운 노드를 삽입할 위치의 이전 노드를 가리키는 포인터이다.
-// Node* node 삽입할 새로운 노드를 가리키는 포인터이다. 
-
-/*void InsertHead(Node** current, Node* head);
-// Node** current 리스트의 헤드 포인터의 주소를 가리키는 포인터입니다.
-// Node* head 리스트에 삽입할 새로운노드의 포인터이다.
-*/
-
-void Remove(Node** head, Node* remove);
-// Node** head 리스트의 헤드 포인터의 주소를 가리키는 포인터이다
-// Node* remove 삭제할 노드의 포인터이다.
-Node* GetNode(Node* head, int index);
-// Node* head 리스트의 시작 노드를 가리키는 포인터이다.
-// int index 가져오고자하는 노드의 인덱스이다.
-// 인덱스는 데이터의 위치를 나타내는 숫자이다. 
-int GetNodeCount(Node* head);
-// 이 함수는 연결리스트의 길이는 반환하는 역할이다.
-// int로 선언 하면 함수가 정수 값을 반환한다는 뜻이다. 
