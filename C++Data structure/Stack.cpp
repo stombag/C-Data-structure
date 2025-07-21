842e57573ee052ee6692b42102680a92388c785a
민혁 김 <ngl570202@gmail.com>
2025-07-21 23:51:09 +0900

    pop 까지 이해 완료

--- C++Data structure/Stack.cpp
+++ C++Data structure/Stack.cpp
@@ -1,76 +1,59 @@
-#include "Stack.h"
-
 #include <iostream>
-
+#include "Stack.h"
 using namespace std;
 
-
-Stack::Stack(int size)
-{
-	capacity = size;
-	arr = new int[capacity];
-	top = -1;
+Stack::Stack(int size) {
+    capacity = size; 
+    arr = new int[capacity]; // 동적메모리 할당 
+    top = -1; // -1을 비어 있다는 뜻으로 의미한다. 에러 신호로 -1를 이용한것이다. 
 }
 
-Stack::~Stack()
-{
-	delete[] arr;
-
+Stack::~Stack() {
+    delete[] arr;  // 메모리 청소
 }
 
-void Stack::push(int value)
-{
-
-	if (isFull())
-	{
-		cout << " 다참 " << endl;
-		return;
-	}
-	arr[++top] = value;
+void Stack::push(int value) {
+    if (isFull()) { 
+        cout << "스택이 가득 찼습니다!" << endl;
+        return;
+    }
+    arr[++top] = value;  // 이 코드가 실행 될때마다 top은 1씩 증가하면 
+    // 배열의 위치가 변경된다. 
 }
 
-
-int Stack::pop()
-{ 
-	if (isEmpty())
-	{
-		cout << "스택이 비어있습니다." << endl;
-		return -1;
-	}
-	return arr[top--];
-
-			
-}
-int Stack::peek() const
-{
-	if (isEmpty())
-	{
-		cout << "비어 있음	" << endl;
-		return -1;	
-	}
-	return arr[top];
-	
+bool Stack::pop(int& result) { // 주소를 전달해서  원본을 바꾼다. 
+    if (isEmpty()) {
+        return false;  // 실패
+    }
+    result = arr[top--]; // 맨위에 워치한 배열을 이동시켜서 연결을 끈고 그 값을 result에 넣는다. 
+    return true;  // 성공
 }
 
-bool Stack::isEmpty() const
-{
-	return top == -1;
+bool Stack::peek(int& result) const {
+    if (isEmpty()) {
+        return false;
+    }
+    result = arr[top];
+    return true;
 }
 
-bool Stack::isFull() const
-{
-	return top == capacity - 1;	
+bool Stack::isEmpty() const {
+    return top == -1;
 }
 
-void Stack::print() const
-{
-	if (isEmpty())
-	{
-		cout << "비어 있는" << endl;
-		return;
+bool Stack::isFull() const {
+    return top == capacity - 1;
 }
-	cout << "스택 내용" << endl;
-	for (int i = top; i >= 0; --i)
-		cout << arr[i] << " ";
-	cout << endl;
+
+void Stack::print() const {
+    if (isEmpty()) {
+        cout << "스택이 비어있습니다!" << endl;
+        return;
+    }
+
+    cout << "스택 (top → bottom): ";
+    for (int i = top; i >= 0; --i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
--- C++Data structure/Stack.h
+++ C++Data structure/Stack.h
@@ -2,18 +2,18 @@
 class Stack
 {
 private:
-	int* arr;
-	int capacity;
-	int top;
+	int* arr;// 스택 배열 
+	int capacity; // 최대 크기
+	int top;// top 인덱스
 
 public:
-	Stack(int size = 100);
-	~Stack();
-	void push(int value);
-	int pop();
-	int peek() const;
-	bool isEmpty() const;
-	bool isFull()const;
-	void print() const;
+	Stack(int size = 100); // 생성자 
+	~Stack(); // 소멸자
+	void push(int value);// 값을 넣기
+	bool pop(int&result);// 값을 꺼내고 지우기
+	bool peek(int& result) const;// 맨위의 값을 보기
+	bool isEmpty() const;// 비어있는지 확인
+	bool isFull()const; // 꽉 찼는지 확인 
+	void print() const;// 스택 출력 
 
 };
