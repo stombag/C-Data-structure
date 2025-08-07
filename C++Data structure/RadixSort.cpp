75b63bd9bd8a31747e4fc9eba7169e0e6021fe40
qkekqkfkathfl@gmail.com <qkekqkfkathfl@gmail.com>
2025-08-07 15:19:55 +0900

    주석 확인~

--- C++Data structure/RadixSort.cpp
+++ C++Data structure/RadixSort.cpp
@@ -30,14 +30,26 @@ int RadixSort::getMax() {
     return maxVal;
 }
 
-void RadixSort::countingSort(int exp) {
-    int buckets[10][100];       // 최대 버킷 크기 100개로 가정
-    int bucketCount[10] = { 0 }; // 각 버킷의 현재 길이
+void RadixSort::countingSort(int exp) { // 이 함수는 현재 자릿수를 기준으로 정렬하는 역할을한다
+    int buckets[10][100];       
+
+    // exp는 비교할 자릿수를 나타내고 그 자릿수에 해당하는 숫자를 기준으로 0 ~ 9번 버킷에 분류해서 저장한다.
+    // 예를 들어 exp = 10이면 십의 자리 수가 2인 20은 buckets[2]에 30은 buckets[3]에 저장된다.
+    // 각 버킷 안의 100 크기 배열에 하나씩 차례로 넣는다. 
+    int bucketCount[10] = { 0 }; // 각 버킷에 들어간 원소 수 
+    // 버킷은 자리수 별로 분류할때 숫자들을 담아두는 임시저장소이다. 
 
     // 버킷에 분류
     for (int i = 0; i < size; ++i) {
         int digit = (arr[i] / exp) % 10;
         buckets[digit][bucketCount[digit]++] = arr[i];
+        
+        // 예)
+        // 170 (170/10)%10 = 7  십의 자리는 7, 들어갈 버킷 buckets[7][0] = 170
+        // 45 (45/10)%10 = 4 , 십의 자리는 4, 들어갈 버킷 buckets[4][0] = 45
+        // 75 (75/10)%10 = 7, 십의 자리는 7, 들어갈 버킷 buckets[7][1] = 75
+        // 버킷 7에는 170, 75, 버킷 4에는 45
+        //
     }
 
     // 버킷에서 꺼내기
@@ -53,7 +65,10 @@ void RadixSort::sort() {
     int maxVal = getMax();
 
     for (int exp = 1; maxVal / exp > 0; exp *= 10) {
-        //
+        // exe = 1 -> 일의 자리 
+        // exe = 10 -> 십의 자리 
+        // exe = 100 -> 백의 자리 
+        // maxval/ exp >0은 최대값이 현재 자릿수보다 작아질 때까지만 반복한다.
         countingSort(exp);
     }
 }
